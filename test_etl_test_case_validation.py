05a3bb2b75ad5d64d7d7343cfbbca101c7e9238b
deepktiwary <139046108+deepktiwary@users.noreply.github.com>
2025-07-08 23:28:35 +0530

    Update test_etl_test_case_validation.py

--- test_etl_test_case_validation.py
+++ test_etl_test_case_validation.py
@@ -33,17 +33,6 @@ select count(*) from {tables}
 ''')[0][0] >= 1
 
 
-@pytest.mark.parametrize('tables',
-                         [
-                             'sales_fact',
-                             'sales_agg'
-                         ]
-                         )
-def test_column_names_in_tables(tables):
-    assert (sorted(extract_columns_from_tables_in_list_form(tables)) ==
-            sorted(extract_columns_from_mapping_doc(tables)))
-
-
 @pytest.mark.parametrize('primary_key, table_name',
                          [
                              ('sales_id', 'sales'),
@@ -60,26 +49,6 @@ def test_duplicate_records_on_the_basis_of_primary_key(primary_key, table_name):
     group by {primary_key}
     having count(*) > 1
 ''') == []
-
-
-@pytest.mark.parametrize('source_column, source_table, target_column, target_table',
-                         [
-                             ('sales_id', 'sales', 'sales_id', 'sales_fact'),
-                             ('date_id', 'date_dim', 'date_id', 'sales_fact'),
-                             ('customer_id', 'customer', 'customer_id', 'sales_fact'),
-                             ('product_id', 'products', 'product_id', 'sales_fact'),
-                             ('purchase_quantity', 'sales', 'quantity', 'sales_fact'),
-                             ('per_unit_price', 'sales', 'unit_price', 'sales_fact'),
-                             ('customer_id', 'customer', 'customer_id', 'sales_agg')
-                         ]
-
-                         )
-def test_to_ensure_no_records_are_dropped(source_column, source_table, target_column, target_table):
-    assert run_sql_query_for_validation(f'''
-    select distinct {source_column} from {source_table}
-    ''') == run_sql_query_for_validation(f'''
-    select distinct {target_column} from {target_table}
-    ''')
 
 
 @pytest.mark.parametrize('sales_id',
